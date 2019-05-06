1b1a1ed127662f7aae93dca49851f3f0940ef18c
davidbisegna <david.bisegna@etu.univ-cotedazur.fr>
2019-05-07 00:27:07 +0200

    Added bluetooth activation button in the Share menu

--- app/src/main/java/com/example/ihm/FragmentShare.java
+++ app/src/main/java/com/example/ihm/FragmentShare.java
@@ -1,15 +1,58 @@
 package com.example.ihm;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import static android.app.Activity.RESULT_OK;
 
 public class FragmentShare extends Fragment {
+    BluetoothAdapter bluetoothAdapter;
+    int REQUEST_ENABLE_BT = 1;
+
+
+    public void handleBluetoothActivation(){
+        int REQUEST_ENABLE_BT = 0;
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         return inflater.inflate(R.layout.fragment_share, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceBundle){
+        super.onActivityCreated(savedInstanceBundle);
+        Button bluetoothButton = getActivity().findViewById(R.id.buttonBluetoothShare);
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bluetoothAdapter.isEnabled()){
+                    handleBluetoothActivation();
+                }
+            }
+        });
+        if(this.bluetoothAdapter == null){
+            bluetoothButton.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(resultCode == RESULT_OK){
+                System.out.println("OK");
+            }
+        }
+    }
+
 }
