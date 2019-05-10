8788972deb7be048cdfb18ec275efbfec6fefa8a
davidbisegna <david.bisegna@etu.unice.fr>
2019-05-11 00:22:19 +0200

    Added Offline Firebase support

--- app/src/main/java/com/example/ihm/GoogleSignInActivity.java
+++ app/src/main/java/com/example/ihm/GoogleSignInActivity.java
@@ -18,6 +18,8 @@ package com.example.ihm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -25,6 +27,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -38,6 +41,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -66,6 +70,7 @@ public class GoogleSignInActivity extends BaseActivity implements
 
         context = this;
 
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
         // Button listeners
         findViewById(R.id.signInButton).setOnClickListener(this);
